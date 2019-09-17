3cc254c1ae169fd0e3d53a6d715e7d77d2469798
Nathaniel <n.maniti@gmail.com>
2019-09-17 05:26:24 +0000

    add modal

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-  	#resources :comments, only: :create
-  	#resources :photos, only: :create
+  	resources :comments, only: :create
+  	resources :photos, only: :create
   end
-  #resources :users, only: :show䀀
+  resources :users, only: :show䀀
 end
