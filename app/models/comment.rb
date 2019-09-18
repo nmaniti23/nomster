46c95b57f6eb2fdd64eee4f71e8f0bac4f172b0a
Nathaniel <n.maniti@gmail.com>
2019-09-18 21:50:53 +0000

    email fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,21 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :place
+	belongs_to :user
+	belongs_to :place
+	after_create :send_comment_email
 
-    RATINGS = {
-    'one star': '1_star',
-    'two stars': '2_stars',
-    'three stars': '3_stars',
-    'four stars': '4_stars',
-    'five stars': '5_stars'
-  }
+	RATINGS = {
+		'*': '1_star',
+		'* *': '2_stars',
+		'* * *': '3_stars',
+		'* * * *': '4_stars',
+		'* * * * *': '5_stars'
+	}
 
-    def humanized_rating
-      RATINGS.invert[self.rating]
-  end
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
+
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver_now
+	end
 end
